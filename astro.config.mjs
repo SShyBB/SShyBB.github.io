efdb276cf6d14f2fec024e15a87465192b5c01ca
Juan Guillermo Iván Ibarra Marmolejo <ivan.shy2001@gmail.com>
2024-05-29 12:59:50 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sshybb.github.io/',
-  base: 'https://github.com/SShyBB/SShyBB.github.io',
   integrations: [tailwind()]
 });
